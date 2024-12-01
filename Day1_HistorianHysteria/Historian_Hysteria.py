2001a926e140126d2829401f7738c32c19904478
Stefan Kranz <kranz.stefan99@gmail.com>
2024-12-01 11:06:11 +0100

    divide first and second riddle

--- Day1_HistorianHysteria/Historian_Hysteria.py
+++ Day1_HistorianHysteria/Historian_Hysteria.py
@@ -3,15 +3,9 @@ import pandas as pd
 
 def main():
     left_list, right_list = read_input("input.txt")
-    left_list.sort()
-    right_list.sort()
 
-    total_distance = 0
-    while len(left_list) != 0 and len(right_list) != 0:
-        distance = abs(left_list.pop() - right_list.pop())
-        total_distance += distance
-
-    print(f"The total distance is {total_distance}")
+    first_star_solution = solve_first_star(left_list, right_list)
+    print(f"The total distance is {first_star_solution}")
 
 
 def read_input(filename):
@@ -20,6 +14,19 @@ def read_input(filename):
     right_list = df[1].tolist()
     return left_list, right_list
 
+def solve_first_star(left_list, right_list):
+    left_list.sort()
+    right_list.sort()
+
+    total_distance = 0
+    while len(left_list) != 0 and len(right_list) != 0:
+        distance = abs(left_list.pop() - right_list.pop())
+        total_distance += distance
+
+    return total_distance
+
+def solve_second_star(left_list, right_list):
+    pass
 
 if __name__ == '__main__':
     main()
