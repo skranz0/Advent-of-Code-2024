7cfcd91e0afcc068e165de2b2c406b0d0dd63112
Stefan Kranz <kranz.stefan99@gmail.com>
2024-12-01 11:02:52 +0100

    ⭐️

--- Day1_HistorianHysteria/Historian_Hysteria.py
+++ Day1_HistorianHysteria/Historian_Hysteria.py
@@ -2,13 +2,24 @@ import pandas as pd
 
 
 def main():
-    location_ids = read_input("input.txt")
-    print(location_ids)
+    left_list, right_list = read_input("input.txt")
+    left_list.sort()
+    right_list.sort()
+
+    total_distance = 0
+    while len(left_list) != 0 and len(right_list) != 0:
+        distance = abs(left_list.pop() - right_list.pop())
+        total_distance += distance
+
+    print(f"The total distance is {total_distance}")
+
 
 def read_input(filename):
     df = pd.read_table(filename, sep='  ', header=None)
-    df.rename(columns={df.columns[0]: 'left_list', df.columns[1]: 'right_list'}, inplace=True)
-    return df
+    left_list = df[0].tolist()
+    right_list = df[1].tolist()
+    return left_list, right_list
+
 
 if __name__ == '__main__':
     main()
