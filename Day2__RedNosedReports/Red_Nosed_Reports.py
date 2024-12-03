34a5f3eec705753d69c61effd93d833f95c97e88
Stefan Kranz <kranz.stefan99@gmail.com>
2024-12-03 15:28:21 +0100

    wip for second star

--- Day2__RedNosedReports/Red_Nosed_Reports.py
+++ Day2__RedNosedReports/Red_Nosed_Reports.py
@@ -3,10 +3,15 @@ def main():
     safe_reports = 0
     unsafe_reports = 0
     for report in reports:
-        if check_consistency(report) and check_differences(report):
+        report_list = list(map(int, report.split()))
+        if check_consistency(report_list) and check_differences(report_list):
             safe_reports += 1
         else:
-            unsafe_reports += 1
+            dampened_report_list = dampen(report_list)
+            if check_consistency(dampened_report_list) and check_differences(dampened_report_list):
+                safe_reports += 1
+            else:
+                unsafe_reports += 1
     print(f"Safe: {safe_reports}")
     print(f"Unsafe: {unsafe_reports}")
 
@@ -16,17 +21,25 @@ def read_input(filepath):
     print(f"{len(lines)} reports read")
     return lines
 
-def check_consistency(report: str) -> bool:
-    report = list(map(int, report.split(' ')))
+def check_consistency(report: list[int]) -> bool:
     return report == sorted(report) or report == sorted(report, reverse=True)
 
-def check_differences(report: str) -> bool:
-    report = list(map(int, report.split()))
+def check_differences(report: list[int]) -> bool:
     for i in range(len(report) - 1):
         difference = abs(report[i] - report[i+1])
         if difference < 1 or difference > 3:
             return False
     return True
+
+def dampen(report: list[int]) -> list[int]:
+    for level in report:
+        dampened_report = report.copy()
+        dampened_report.remove(level)
+        if check_consistency(dampened_report) and check_differences(dampened_report):
+            return dampened_report
+    return report
+
+
 
 
 if __name__ == '__main__':
