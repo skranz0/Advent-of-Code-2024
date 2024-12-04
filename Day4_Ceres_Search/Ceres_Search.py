d6871b42a9af265f08e065c0b7c1c2af790af43f
Stefan Kranz <kranz.stefan99@gmail.com>
2024-12-04 13:35:06 +0100

    works for test

--- Day4_Ceres_Search/Ceres_Search.py
+++ Day4_Ceres_Search/Ceres_Search.py
@@ -2,10 +2,12 @@ import numpy as np
 
 
 def main():
-    word_search = read_input("test.txt")
+    word_search = read_input("input.txt")
     #print(word_search)
     horizontal = search_horizontal(word_search)
     vertical = search_vertical(word_search)
+    diagonal = search_diagonal(word_search)
+    print(f"{horizontal} + {vertical} + {diagonal} = {horizontal + vertical + diagonal}")
 
 def read_input(filename):
     with open(filename, 'r') as f:
@@ -27,7 +29,15 @@ def search_vertical(word_search):
     count = search_horizontal(word_matrix)
     return count
 
-
+def search_diagonal(word_search):
+    word_matrix = np.asmatrix(word_search)
+    word_matrix_t = word_matrix.transpose()
+    diagonals, diagonals_t = [[],[]]
+    for i in range(-len(word_matrix), len(word_matrix)):
+        diagonals.append(np.diag(word_matrix, i).tolist())
+        diagonals_t.append(np.diag(word_matrix_t, i).tolist())
+    count = search_horizontal(diagonals) + search_horizontal(diagonals_t)
+    return count
 
 if __name__ == '__main__':
     main()
